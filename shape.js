690d765ac0f40ff8f4accc2d829e54e02d6d6c2e
vivek-hash <63544533+vivek-hash@users.noreply.github.com>
2021-11-17 18:04:07 +0530

    Update and rename inheritance to shape.js

---
+++ shape.js
@@ -0,0 +1,13 @@
+class Shape{
+  constructor(color){
+    this.color= 'color';
+  }
+
+     drawShape(){
+    console.log('Shape');
+     }
+  calculateArea(){
+    console.log('Area');
+     }
+}
+module.exports = Shape;
